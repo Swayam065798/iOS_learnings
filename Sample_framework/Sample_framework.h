d6809d549ef222177f6acc192c2cb5774ea7fdc6
Swayam <swayamdiptab@gmail.com>
2025-08-16 20:33:21 +0530

    Initial Commit

---
+++ Sample_framework/Sample_framework.h
@@ -0,0 +1,18 @@
+//
+//  Sample_framework.h
+//  Sample_framework
+//
+//  Created by Comviva on 16/08/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Sample_framework.
+FOUNDATION_EXPORT double Sample_frameworkVersionNumber;
+
+//! Project version string for Sample_framework.
+FOUNDATION_EXPORT const unsigned char Sample_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sample_framework/PublicHeader.h>
+
+
