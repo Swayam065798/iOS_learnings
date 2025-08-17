704a0caf2d053c7a9510504c5d84a17d80397610
Swayam <swayamdiptab@gmail.com>
2025-08-17 14:19:51 +0530

    Added latest changes for the framework learning

---
+++ Sample_framework/HelloWorld.swift
@@ -0,0 +1,10 @@
+//
+//  HelloWorld.swift
+//  Sample_framework
+//
+//  Created by Comviva on 17/08/25.
+//
+// to simulate the behaviour of acccess specifiers
+import Foundation
+
+
---
+++ Sample_framework/Sample.swift
@@ -0,0 +1,16 @@
+//
+//  Sample.swift
+//  Sample_framework
+//
+//  Created by Comviva on 16/08/25.
+//
+
+import Foundation
+
+public class Sample {
+    public init() {}
+    
+    public func printMessage(messageString : String) {
+        print(messageString)
+    }
+}
